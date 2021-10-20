89f74f64d99eb32bb086e8e03ddac2823c7d1348
MattiaM99 <mattiamazzoni123@gmail.com>
2021-10-20 16:55:41 +0200

    fine parte senza bonus

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+stampa nomecognomecolorepreferito21
+*/
+
+const firstname = prompt("Inserisci il nome");
+const lastname = prompt("Inserisci il cognome");
+const favouritecolor = prompt("Qual è il tuo colore preferito?");
+
+console.log(firstname);
+console.log(lastname);
+console.log(favouritecolor);
+
+const textToPrint =
+`
+  La tua password consigliata è: ${firstname}${lastname}${favouritecolor}21
+`;
+
+console.log(textToPrint);
+document.getElementById('password').innerHTML = textToPrint;
